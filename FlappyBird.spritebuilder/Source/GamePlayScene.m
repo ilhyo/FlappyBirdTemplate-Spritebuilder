4448a2f01bb83a89360e31a07ee2aa5c2d52e54b
ilhyo <ilhyo@naver.com>
2015-06-28 16:20:10 +0900

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,8 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +16,8 @@
     // put update code here
 }
 
-// put new methods here
-
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    [character flap];
+}
 @end
